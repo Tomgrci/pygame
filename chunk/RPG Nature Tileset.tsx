007decff20ced5d8b2522d474ca5f353c568017f
Tomgrci <65333929+Tomgrci@users.noreply.github.com>
2020-05-14 09:43:38 +0200

    Add files via upload

---
+++ chunk/RPG Nature Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="RPG Nature Tileset" tilewidth="64" tileheight="64" tilecount="180" columns="20">
+ <image source="../image/tiles/RPG Nature Tileset.png" width="1282" height="576"/>
+</tileset>
