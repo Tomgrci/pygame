9b22fb101a1660c8c832dd59cdab24bd9ccbdc8a
Tomgrci <65333929+Tomgrci@users.noreply.github.com>
2020-06-05 19:09:36 +0200

    Add files via upload

---
+++ chunk/portal.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="portal" tilewidth="64" tileheight="64" tilecount="9" columns="3">
+ <image source="../../v0.3/image/perso/portal.png" width="200" height="200"/>
+</tileset>
